1925c5edc6316c6adc3fbcdd09bad2d7628290c0
vivian_xu <540978449@qq.com>
2016-11-28 17:07:37 +0800

    分为 生产环境和 发布环境， 启用sass 弃用 ruby sass, 弃用compass

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+    minimist = require('minimist'), // 环境变量
     sass = require('gulp-sass'),
     rubySass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
@@ -11,30 +12,54 @@ var gulp = require('gulp'),
 
     htmlmin = require('gulp-htmlmin'),
 
-    gutil = require('gulp-util'),
     concat = require('gulp-concat'), // 合并文件
     rename = require('gulp-rename'), //重命名文件
     connect = require('gulp-connect'),
 
+    gulpif = require('gulp-if'),
 
-    gulpif = require('gulp-if');
-    
-    var env = process.env.NODE_ENV || 'development';
+    gutil = require('gulp-util'),
+    plumber = require('gulp-plumber'),
+    handleErrors = require('./gulp/handleErrors');
+
+    // 环境变量设置
+    var knownOptions = {
+      string: 'env',
+      default: { env: process.env.NODE_ENV || 'development' }
+    };
+
+    var options = minimist(process.argv.slice(2), knownOptions);
+
+    var isProduct = options.env === 'production';
+
+    // 环境变量设置 完
+
     var outputDir = 'dist/development';
 
     var config = {};
-    
-    if (env === "development") {
+
+    if ( !isProduct ) {
         config.map = 'sourcemaps.write()';
         config.outputStyle= '';
         config.outputdir = 'dist/development';
-    }
-
-    else if (env === "production") {
+    } else {
         config.map = '';
         config.outputStyle = '{outputStyle: "compressed"}';
-        config.outputdir = 'dis/production';
+        config.outputdir = 'dist/production';
     }
+
+    config.autoprefixer = {
+      browsers: [
+        'last 2 versions',
+        'safari 5',
+        'ie 8',
+        'ie 9',
+        'opera 12.1',
+        'ios 6',
+        'android 4'
+      ],
+      cascade: true
+    };
 
 
     gulp.task('html', function() {
@@ -44,69 +69,82 @@ var gulp = require('gulp'),
                 path.basename += '.min';
             }))
             .pipe(gulp.dest(config.outputdir+'/html'))
-            .pipe(connect.reload());
+            // .pipe(connect.reload());
+            .pipe(gulpif( !isProduct, connect.reload()));
     })
 
     gulp.task('js', function() {
-        return gulp.src('src/js/*.js')
-            .pipe(sourcemaps.init({loadMaps: true}))
-            .pipe (jsmin().on('error', gutil.log))
-            .pipe(sourcemaps.write('./'))
-            .pipe(rename( function( path ) {
-                path.basename += '.min';
-            }))
-            .pipe(gulp.dest(config.outputdir+'/js'))
-            .pipe(connect.reload());
+
+        if ( !isProduct ) {
+            return gulp.src('src/js/*.js')
+                .pipe(plumber( { errorHandler: handleErrors() }))
+                .pipe(concat('main.js'))
+                .pipe(sourcemaps.init({loadMaps: true}))
+                .pipe(sourcemaps.write('./'))
+                .pipe(gulp.dest(config.outputdir+'/js'))
+                .pipe(connect.reload());
+
+        } else {
+            console.log("js in production");
+            return gulp.src('src/js/*.js')
+                .pipe(concat('main.js'))
+                .pipe (jsmin())
+                .pipe(rename( function( path ) {
+                    path.basename += '.min';
+                }))
+                .pipe(gulp.dest(config.outputdir+'/js'))
+        }
     });
-    
+
     gulp.task('sass', function() {
-        return gulp.src('src/sass/*.scss')
+
+        if ( !isProduct ) {
+
+            return gulp.src('src/sass/*.scss')
             .pipe(sourcemaps.init())
             .pipe(sass(config.outputStyle).on("error", sass.logError))
-            .pipe(autoprefixer())
-            .pipe(config.map)
-            .pipe(rename( function( path ) {
-                path.basename += '.min';
-            }))
+            .pipe(autoprefixer(config.autoprefixer))
             .pipe(gulp.dest(config.outputdir+ '/css'))
             .pipe(connect.reload());
-     });
 
+        } else {
+
+            return gulp.src('src/sass/*.scss')
+                .pipe(sourcemaps.init())
+                .pipe(sass(config.outputStyle).on("error", sass.logError))
+                .pipe(autoprefixer(config.autoprefixer))
+                .pipe(rename( function( path ) {
+                    path.basename += ".min"
+                }))
+                .pipe(gulp.dest(config.outputdir+ '/css'));
+        }
+    });
 
      gulp.task('rubySass', function() {
-        var rboutputStyle = '',
-            rbname = '';
-        if (env === "development") {
-            rboutputStyle = '';
-            rbrename = "";
-        }
 
-        else if (env === "production") {
-            rboutputStyle = 'compressed';
-            rbrename = ".min";
-        }
+         if ( !isProduct ) {
 
-        return rubySass('src/sass/*.scss', { sourcemap: true ,compass: true, style: rboutputStyle})
-            .on('error', rubySass.logError)
-            .pipe(sourcemaps.write())
+            return rubySass('src/sass/*.scss', { sourcemap: true,
+                        })
+                .on('error', rubySass.logError)
+                .pipe(sourcemaps.write('maps', {
+                    includeContent: false,
+                    sourceRoot: 'source'
+                }))
+                .pipe(autoprefixer(config.autoprefixer))
+                .pipe(gulp.dest(config.outputdir+ '/css'))
+                .pipe(connect.reload());
+         } else {
 
-            .pipe(sourcemaps.write('maps', {
-                includeContent: false,
-                sourceRoot: 'source'
-            }))
-            .pipe(rename( function( path ) {
-                path.basename += rbname 
-            }))
-            .pipe(gulp.dest(config.outputdir+ '/css'))
-            .pipe(connect.reload());
-     
-     })
-
-//    gulp.task('autoprefixer',function() {
-//        return gulp.src('src/css/*.css')
- //           .pipe(autoprefixer()) 
- //           .pipe(gulp.dest('dist/development/css'));
- //   });
+            return rubySass('src/sass/*.scss', { sourcemap: false ,
+                        compass: true})
+                .pipe(autoprefixer(config.autoprefixer))
+                .pipe(rename( function( path ) {
+                    path.basename += ".min"
+                }))
+                .pipe(gulp.dest(config.outputdir+ '/css'));
+         }
+     });
 
     gulp.task('image',function() {
         return gulp.src('src/images/*.{png,jpg,gif,ico}')
@@ -114,9 +152,9 @@ var gulp = require('gulp'),
                 progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
                 svgoPlugins: [{removeViewBox: false}], //不要移除svg的viewbox属性
                 use: [pngquant()] //使用pngquant深度压缩png图片的imagemin插件
-            })) 
+            }))
             .pipe(gulp.dest(config.outputdir+'/images'))
-            .pipe(connect.reload());
+            .pipe(gulpif( !isProduct, connect.reload()));
     });
 
 //  只压缩修改的图片,没有修改的图片直接从缓存文件读取
@@ -128,18 +166,25 @@ var gulp = require('gulp'),
 //
 
     gulp.task('watch', function() {
-        gulp.watch('src/templates/*.html', ['html']);
-        gulp.watch('src/js/*.js', ['js']);
-        gulp.watch('src/sass/*.scss', ['rubySass']);
+        if( !isProduct ) {
+            gulp.watch('src/templates/*.html', ['html']);
+            gulp.watch('src/js/*.js', ['js']);
+            gulp.watch('src/sass/*.scss', ['rubySass']);
+        } else {
+            return;
+        }
     });
 
     gulp.task('connect', function() {
-        connect.server({
-            root: [outputDir],
-            port: 8000,
-            livereload: true
-        });
+        if( !isProduct ) {
+            return connect.server({
+                root: [outputDir],
+                port: 8080,
+                livereload: true
+            });
+        } else {
+            return;
+        }
     });
 
-    gulp.task('default', ['html', 'js', 'rubySass', 'image', 'watch', 'connect']);
-
+    gulp.task('default', ['html', 'js', 'sass', 'image', 'watch', 'connect']);
