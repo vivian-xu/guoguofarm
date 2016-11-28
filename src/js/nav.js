20ba6eefc24b94cffabc62f1fb6cd4aeae29b20a
vivian_xu <540978449@qq.com>
2016-11-28 17:12:58 +0800

    不要 nav 的变色

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,23 +1,17 @@
-$(document).ready(function() {
+
     $(".item_downMenu").click( function(){
         $(".menu_item").fadeToggle(500);
-  });
+    });
 
-    var top_nav = $("#top-nav.hide");
-    $(window).scroll(function() {
-     var rol_height = $(document).scrollTop();
-      if ( rol_height > 10) { top_nav.removeClass("hide"); }
-      else { top_nav.addClass("hide"); }
- });
-
-    $("#top-nav #nav-back").on('click', function(event) {
-    event.preventDefault();
-    /* Act on the event */
-    window.history.back();
+    $("#nav-back").on('click', function(event) {
+      event.preventDefault();
+      console.log('back');
+      /* Act on the event */
+      window.history.back();
     });
 
     $("#top-nav #nav-share").on('click', function(event) {
     	event.preventDefault();
     	/* Act on the event */
     });
-})
+
